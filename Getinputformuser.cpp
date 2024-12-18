fbb99ab3984fc04892d2a0f906bc5406fa06bd8a
Afzal khan <afzalkhan01765@gmail.com>
2024-12-18 11:28:15 +0600

    add

---
+++ Getinputformuser.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double  n,m ,sum ;
+    cout<<"Enter the Value of a: ";cin>>n;
+    cout<<"Enter the value of b:";cin>>m;
+    sum = n + m ;
+    cout<<"The sumation value of n and m is :"<< n + m;
+
+ return 0;
+
+}
