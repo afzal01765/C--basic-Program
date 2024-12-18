1dd39a756400c84160bd7f0ac0efded3cf4f63e1
Afzal khan <afzalkhan01765@gmail.com>
2024-12-18 11:40:43 +0600

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b;
+    cout<<"Enter the value of a:"<<endl;cin>>a;
+    cout<<"Enter the valu of b:"<<endl;cin>>b;
+    int temp = a;
+    a = b ;
+    b = temp;
+    cout<<"The value of a is :"<<a<<endl;
+    cout<<"The value of b is :"<<b<<endl;
+
+
+
+  return 0;
+}
