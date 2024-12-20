41a819194cf0bea5db2118e9d9431e86368a0af6
Afzal khan <afzalkhan01765@gmail.com>
2024-12-20 09:33:14 +0600

    Create multipilcation_of_float.cpp

---
+++ multipilcation_of_float.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float a,b,product;cin>>a>>b;
+
+    product = a * b;
+
+    cout<<"The product of two numbers is :"<<product<<endl;
+    return 0;
+
+}
