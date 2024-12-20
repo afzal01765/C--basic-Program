4a2d42515705d30961e70df292807a7278902213
Afzal khan <afzalkhan01765@gmail.com>
2024-12-20 09:26:45 +0600

    add

---
+++ Basic.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace  std;
+int main(){
+
+    int n;
+    float m;
+    double r;
+    char c ;
+    cout<<"The size of int number is:"<<sizeof(n)<<endl;
+    cout<<"The size  of float value is:"<<sizeof(m)<<endl;
+    cout<<"The size of double value is:"<<sizeof(r)<<endl;
+    cout<<"The size of char value is :"<<sizeof(c)<<endl;
+
+ return 0;
+}
